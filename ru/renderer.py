f7b0490b30beafa811f5c33ccac06a99d581de66
hit9 <nz2324@126.com>
2013-08-10 22:44:01 +0800

    add renderer

---
+++ ru/renderer.py
@@ -0,0 +1,45 @@
+# coding=utf8
+
+"""render data with jinja2 template"""
+
+from . import charset
+from .exceptions import JinjaTemplateNotFound
+
+from jinja2 import Environment,FileSystemLoader
+from jinja2.exceptions import TemplateNotFound
+
+
+class Renderer(object):
+
+    def initialize(self, templates_path, global_data):
+        """initialize with templates' path
+        parameters
+          templates_path    str    the position of templates directory
+          global_data       dict   globa data can be got in any templates"""
+        self.env = Environment(loader=FileSystemLoader)
+        self.env.trim_blocks = True
+        self.global_data = global_data
+
+    def render(self, template, **data):
+        """Render data with template, return html unicodes.
+        parameters
+          template   str  the template's filename
+          data       dict the data to render
+        """
+        # make a copy and update the copy
+        dct = self.global_data.copy()
+        dct.update(data)
+
+        try:
+            html = self.env.get_template(template).render(**dct)
+        except TemplateNotFound:
+            raise JinjaTemplateNotFound
+        return html
+
+    def render_to(self, path, template, **data):
+        """Render data with template and then write to path"""
+        html = self.render(template, **data)
+        return open(path, "w").write(html.encode(charset))
+
+
+renderer = Renderer()  # initialized a renderer, and use it each time
