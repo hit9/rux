3d894080d57abe1593d3e701a20939ba0936921c
hit9 <nz2324@126.com>
2013-09-28 17:44:15 +0800

    fix ru=>rux

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
-from ru import version
+from ru import __version__
 
 
 setup(
     name='ru',
-    version=version,
+    version=__version__,
     author='hit9',
     author_email='nz2324@126.com',
-    description='Minimal static blog generator.',
+    description='''A simple, micro and lightweight static site generator,
+    built for mini needs personal blog.''',
     license='BSD',
-    keywords='static blog generator, markdown, toml, posts',
+    keywords='static blog generator, markdown, html',
     url='http://github.com/hit9/ru',
     packages=['ru'],
     include_package_data=True,
@@ -21,12 +22,6 @@ setup(
     },
     install_requires=open("requirements.txt").read().splitlines(),
     dependency_links=[
-        # 'https://github.com/mitsuhiko/jinja2/zipball/master#egg=jinja2-2.6',
-        # 'https://pypi.python.org/packages/source/P/Pygments/Pygments-1.6.tar.gz',
-        # 'https://pypi.python.org/packages/source/b/blinker/blinker-1.2.tar.gz',
-        # 'https://github.com/docopt/docopt/zipball/master#egg=docopt-0.6.1',
-        # 'https://github.com/FSX/houdini.py/zipball/master#egg=houdini.py-0.1.0',
-        # 'https://github.com/FSX/misaka/zipball/master#egg=misaka-1.0.2',
         'https://pypi.python.org/packages/source/b/blinker/blinker-1.2.tar.gz#md5=6b0a876f0778084e97935a951ea96ded',
         'https://github.com/hit9/toml.py/zipball/master#egg=toml.py-0.1.2',
         'https://github.com/sramana/pyatom/archive/master.zip#egg=pyatom-1.3',
