b527db37a1b7e026b3bc9269dea7fd1204800b04
hit9 <nz2324@126.com>
2013-09-28 17:03:10 +0800

    fix some styles

--- ru/__init__.py
+++ ru/__init__.py
@@ -11,15 +11,17 @@
     Rux
     ~~~
 
-    A simple, micro and minimal site generator.
+    A simple, micro and lightweight static site generator, built for mini
+    needs personal blog.
 
     :author: hit9
+    :email: nz2324[at]126.com
     :license: BSD
 """
 
-version = "0.3.0"
-charset = "utf8"
-src_ext = ".md"
-out_ext = ".html"
-src_dir = "src"
-out_dir = "."
+version = '0.3.0'
+charset = 'utf8'  # utf8 input & output
+src_ext = '.md'
+out_ext = '.html'
+src_dir = '.src'
+out_dir = '.'
