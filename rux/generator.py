fde370c6092c581a881295e0e01eb8bcf0f0735a
hit9 <nz2324@126.com>
2013-12-07 18:53:35 +0800

    update to v0.5.5

--- rux/generator.py
+++ rux/generator.py
@@ -9,7 +9,6 @@
 
 from datetime import datetime
 import gc
-import multiprocessing
 from os import listdir as ls
 from os.path import exists
 import sys
@@ -35,46 +34,9 @@ def render_to(path, template, **data):
         sys.exit(e.exit_code)
 
 
-def parse_post(post):
-    """woker function for pool. to parse single post"""
-
-    with open(post.filepath, 'r') as f:
-        content = f.read()
-
-    try:
-        data = parser.parse(content)
-    except ParseException, e:
-        logger.warn(e.__doc__ + ', filepath %r' % post.filepath)
-        pass  # skip
-    else:
-        post.__dict__.update(data)  # set attributes: html, markdown
-
-    return post
-
-
-def render_page(page):
-    """woker function for pool. to render single page and posts inside it"""
-
-    for post in page.posts:
-        render_to(post.out, Post.template, post=post)
-    render_to(page.out, Page.template, page=page)
-
-
 class Generator(object):
-    """
-    This is the core builder, parse markdown source and render html with
-    jinja2 templates.
 
-    We use at most 4(cpu numbers) to build posts
-
-        1. sort source fils by its created time
-        2. parse all posts with 4 processes
-        3. chunk posts to pages, group all pages into 4 groups
-        4. render posts & pages with the same 4 processes
-    """
-
-    POSTS_COUNT_EACH_PAGE = 15  # each page has 15 posts at most
-    BUILDER_PROCESS_COUNT = multiprocessing.cpu_count()
+    POSTS_COUNT_EACH_PAGE = 15
 
     def __init__(self):
         self.reset()
@@ -83,6 +45,10 @@ class Generator(object):
         self.config = config.default
         self.author = author
         self.blog = blog
+        self.posts = []
+        self.pages = []
+
+        gc.collect()
 
     def initialize(self):
         """Initialize configuration and renderer environment"""
@@ -119,96 +85,83 @@ class Generator(object):
         source_files = [join(Post.src_dir, fn)
                         for fn in ls(Post.src_dir) if fn.endswith(src_ext)]
 
-        posts = []
-
         for filepath in source_files:
             try:
                 data = parser.parse_filename(filepath)
             except ParseException as e:  # skip single post parse exception
                 logger.warn(e.__doc__ + ', filepath: %r' % filepath)
             else:
-                posts.append(Post(**data))
+                self.posts.append(Post(**data))
 
-        return posts
-
-    def deal_posts(self, posts):
 
         # sort posts by its created time, from new to old
-        posts.sort(key=lambda post: post.datetime.timetuple(), reverse=True)
+        self.posts.sort(key=lambda post: post.datetime.timetuple(), reverse=True)
 
-        # set attributes `next` and `prev` for each post
-        length = len(posts)
+        count = len(self.posts)
 
-        for idx, post in enumerate(posts):
+        for idx, post in enumerate(self.posts):
 
-            if idx == 0 :
-                setattr(post, 'next', None)
+            if idx == 0:
+                _next = None
             else:
-                setattr(post, 'next', posts[idx-1])
+                _next = self.posts[idx-1]
 
-            if idx == length - 1:
-                setattr(post, 'prev', None)
+            if idx == count - 1:
+                _prev = None
             else:
-                setattr(post, 'prev', posts[idx+1])
+                _prev = self.posts[idx+1]
 
-        return posts
+            setattr(post, 'next', _next)
+            setattr(post, 'prev', _prev)
 
-    def get_pages(self, posts):
-        groups = chunks(posts, self.POSTS_COUNT_EACH_PAGE)
-        pages = [Page(number=idx, posts=list(group)) for idx, group in enumerate(groups, 1)]
+    def parse_posts(self):
 
-        if pages:
-            pages[0].first = True
-            pages[-1].last = True
+        for post in self.posts:
 
-        return pages
+            with open(post.filepath, 'r') as file:
+                content = file.read()
 
+            try:
+                data = parser.parse(content)
+            except ParseException, e:
+                logger.warn(e.__doc__ + ', filepath %r' % post.filepath)
+                pass
+            else:
+                post.__dict__.update(data)
 
-    def generate(self):
-        start_time = time.time()
+    def get_pages(self):
 
-        # -----------------  {{initialization
-        self.initialize()
-        n = self.BUILDER_PROCESS_COUNT
-        # ---------------}}
+        groups = chunks(self.posts, self.POSTS_COUNT_EACH_PAGE)
+        self.pages = [Page(number=idx, posts=list(group))
+                      for idx, group in enumerate(groups, 1)]
 
-        # -----------------  {{ parsing
-        posts = self.get_posts()
-        pool = multiprocessing.Pool(n)
-        pool.apply(parse_post, args=posts)  # parse posts
+        if self.pages:
+            self.pages[0].first = True
+            self.pages[-1].last = True
 
-        # ----------------------------- }}
+    def render(self):
 
-        # deal with posts
-        self.deal_posts(posts)
-
-        # -----------------  {{ rendering
-        pages = self.get_pages(posts)
-        # if `post/` or `page/` not exist, mkdir
         mkdir_p(Post.out_dir)
         mkdir_p(Page.out_dir)
 
-        pool.map(render_page, pages)
+        for page in self.pages:
+            for post in page.posts:
+                render_to(post.out, Post.template, post=post)
+            render_to(page.out, Page.template, page=page)
 
-        # ----------------------------- }}
+    def generate(self):
+        start_time = time.time()
+        self.initialize()
+        self.get_posts()
+        self.get_pages()
+        self.parse_posts()
+        self.render()
 
-        pool.close()
-        pool.join()
-
-        # cleanup memory
-        del posts[:]
-        del posts
-        del pages[:]
-        del pages
-
-        logger.success("Build done with %d process in %.3f seconds" % (
-            n, time.time() - start_time))
+        logger.success("Build done in %.3f seconds" % (time.time() - start_time))
 
     def re_generate(self):
         self.reset()
         self.generate()
-        # gc
-        gc.collect()
 
 
 generator = Generator()
