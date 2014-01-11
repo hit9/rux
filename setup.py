eb81ac3f8ebccd684308afaef69a93b20727d835
hit9 <nz2324@126.com>
2014-01-11 23:08:11 +0800

    docstring for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,45 @@
 # coding=utf8
 
-'''
-Setup script for rux with the ugly setuptools.
-'''
+"""
+Rux
+---
 
+Micro & Fast static blog generator (markdown => html).
 
-import os
+Features
+````````
+
+* Static: Markdown => HTML
+* Not tags, No categories, No feed generation, No ...
+* Minimal & Simple configuration
+* Ability to run in the background as a daemon
+* Ability to save posts in PDF for offline reading
+* Ability to build automatically once source updated
+
+Installation
+`````````````
+
+.. code:: bash
+
+    $ mkdir MyBlog
+    $ cd MyBlog
+    $ virtualenv venv
+    New python executable in venv/bin/python
+    Installing setuptools............done.
+    Installing pip...............done.
+    $ . venv/bin/activate
+    $ pip install rux
+
+Links
+`````
+
+* Documentation <http://rux.readthedocs.org>
+* Code on GitHub <https://github.com/hit9/rux>
+
+**NOTICE**: Rux may not be stable before version 1.0
+
+"""
+
 from setuptools import setup, Extension
 from rux import __version__
 
@@ -32,4 +66,5 @@ setup(
         'https://github.com/hit9/toml.py/zipball/master#egg=toml.py-0.1.2',
     ],
     ext_modules=[Extension('ruxlibparser', ['src/libparser.c'])],
+    long_description=__doc__
 )
