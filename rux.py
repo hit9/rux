d4fdd9a84bcee1fd7b85282a7e5f00fd5e1d5b1f
hit9 <hit9@icloud.com>
2019-08-03 23:23:47 +0800

    Starts the rewrite

--- rux.py
+++ rux.py
@@ -11,135 +11,49 @@
 Rux
 ~~~
 
-Minimal static blog generator (markdown => html).
+Simple static site generator for my blog.
 
-## Features
-
-* Not tags, No categories, No feed generation, ...
-* Simple configurations.
-* Ability to run in the background as a daemon.
-* Ability to save posts in PDF for offline reading.
-* Ability to build automatically once source updated
-
-## Howtos
-
-1. Creates a blog:
-
-    $ mkdir blog && cd blog
-    $ rux init
-
-
-2. Starts a rux daemon:
-
-    $ rux stop
-
-3. Writes a blog post:
-
-    $ rux post
-    $ vim src/...md
-
-4. Preview webpages at https://0.0.0.0:8888
-5. Stop the daemon:
-
-    $ rux stop
-
-
-## Sample Post
-
-Head and body, splits by `---`:
-
-    The title
-    An optional banner picture
-    ---
-    Markdown content..
-
-## Other Commands
-
-1. `rux pdf` generates a pdf of all posts.
-2. `rux serve` starts a simple http server (not daemon).
-3. `rux clean` removes all generated html files.
-
-## Themes
-
-Uses git submodule to manage themes:
-
-    $ git submodule add git://github.com/hit9/rux-theme-default.git default
-
-Available themes:
-
-    * https://github.com/hit9/rux-theme-default
-    * https://github.com/hit9/rux-theme-clr
-
-## Requirements
-
-Python3.4+
-
-## License
-
-BSD, Chao Wang <hit9@icloud.com>
+:copyright: (c) 2014-2019 Chao Wang <hit9@icloud.com>
+:license: BSD.
 
 """
 
-__version__: str = "0.7.0"
-
-
-from typing import Dict, Any, MutableMapping, Callable
+from typing import Callable, Any, Dict
+import os
+import yaml
 from enum import Enum
 import logging
 import datetime
-import toml
+from dataclasses import dataclass
+import hashlib
+
+import mistune
+import pygments
+
+__version__ = "0.7.0"
 
 
-####
+###
 # Utils
 ###
 
 
-class Color(Enum):
-    """Color is an enum collection of colors.
-    The value of color enum item is the ansi escape code for the color.
-    References: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors.
-    """
-
-    BLACK = 30
-    RED = 31
-    GREEN = 32
-    YELLOW = 33
-    BLUE = 34
-    MAGENTA = 35
-    CYAN = 36
-    WHITE = 37
-    BGRED = 41
-    BGGREY = 100
-
-
-def colored_string(color: Color, text: str) -> str:
-    """Returns a colored version of given `text` by given `color`.
-
-        >>> from rux import Color, colored_string
-        >>> print(colored_string(Color.RED, "hello world"))
-
-    """
-    return "\033[{0}m{1}\033[0m".format(color.value, text)
-
-
 class cached_property:
-    """Decorates the given function ``func`` to cache its result at the first call.
-
-    :param func: The target function to decorate.
+    """cached_property is a decorator to decorate given function ``func`` to be a property
+    and cache its result in the memory at the first call.
 
     Usage::
 
         >>> class Foo:
-              @cached_property
-              def some_attribute(self):
+            @cached_property
+            def attr(self) -> int:
                 print("Called!")
                 return sum(range(10))
         >>> foo = Foo()
-        >>> foo.some_attribute
+        >>> foo.attr
         Called!
         45
-        >>> foo.some_attribute
+        >>> foo.attr
         45
 
     """
@@ -155,49 +69,62 @@ class cached_property:
         return val
 
 
+class Color(Enum):
+    """Color is an enum collection of console color codes. """
+
+    BLACK = 30
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+    MAGENTA = 35
+    CYAN = 36
+    WHITE = 37
+    BGRED = 41
+    BGGREY = 100
+
+
+def colored_text(text: str, color: Color) -> str:
+    """Returns a colored version of given ``text``.
+    """
+    return "\033[%dm%s\033[0m".format(text, color.value)
+
+
 ###
 # Logging
 ###
 
 
 class ColoredFormatter(logging.Formatter):
-    """ColoredFormatter is an implementation of colored logging formatter."""
-
-    #: color_mappings is the mappings from logging levels to color names.
-    COLOR_MAPPINGS: Dict[str, Color] = {
-        "CRITICAL": Color.BGRED,
-        "ERROR": Color.RED,
-        "WARNING": Color.YELLOW,
-        "SUCCESS": Color.GREEN,
-        "INFO": Color.CYAN,
-        "DEBUG": Color.BGGREY,
-    }
+    """ColoredFormatter inherits from :class:`logging.Formatter` and implements a custom
+    logging formatter for colored logging.
+    """
 
     def format(self, record: logging.LogRecord) -> str:
-        """Overrides `logging.Formatter` to implement a custom colored formatter.
-        """
+        """Overrides upper :meth:`Formatter.format()`."""
         message: str = record.getMessage()
-        # Gets properly color for this record by its level, defaults to `Color.WHITE`.
-        color: Color = self.COLOR_MAPPINGS.get(record.levelname, Color.WHITE)
-        # Formats level string with paddings.
-        level: str = colored_string(color, "%-8s" % record.levelname)
-        # Formats time now.
-        now: str = colored_string(
-            Color.MAGENTA, datetime.datetime.now().strftime("%H:%M:%S")
+        #: mapping is a map from logging levelname to color enums.
+        mapping: Dict[str, Color] = {
+            "CRITICAL": Color.BGRED,
+            "ERROR": Color.RED,
+            "WARNING": Color.YELLOW,
+            "INFO": Color.GREEN,
+            "DEBUG": Color.BGGREY,
+        }
+        color: Color = mapping.get(record.levelname, Color.WHITE)
+        level_str: str = colored_text("%-8s" % record.levelname, color)
+        time_str: str = colored_text(
+            datetime.datetime.now().strftime("(%H:%M:%S)"), Color.MAGENTA
         )
-        # Upper format of logger's name.
-        name_upper: str = record.name.upper()
-        return " :: ".join([name_upper, level, now, message])
+        return " ".join([level_str, time_str, message])
 
 
-#: `logger` is the global logger.
-logger: logging.Logger = logging.getLogger(__name__)
-#: `logger_console_handler` is the logging handler that logs to current console.
+#: ``logger`` is the global logger.
+logger: logging.Logger = logging.getLogger("rux")
 logger_console_handler: logging.Handler = logging.StreamHandler()
+logger_console_handler.setLevel(logging.INFO)
 logger_console_handler.setFormatter(ColoredFormatter())
 logger.addHandler(logger_console_handler)
-# Using level `INFO`.
-logger.setLevel(logging.INFO)
 
 
 ###
@@ -206,56 +133,176 @@ logger.setLevel(logging.INFO)
 
 
 class Configs:
-    """Configs is the abstraction of rux blog's `config.toml`.
+    """Configs is the abstraction of rux's configuration file ``config.yaml``.
     """
 
-    #: configs_filename is the file's name of target configuration file.
-    CONFIGS_FILENAME: str = "config.toml"
+    #: `configs_filename` is the file name of the configuration file.
+    configs_filename: str = "config.yaml"
+    #: `configs_filepath` is the absolute path of the configuration file.
+    configs_filepath: str = os.path.join(os.getcwd(), configs_filename)
 
     def __init__(self) -> None:
-        #: ``configs`` is the internal configuration holder for the data loaded
-        #: from configuration file.
-        self.configs: MutableMapping[str, Any] = {}
+        #: Attribute `configs` holds the original parsed yaml dict from the configuration
+        #: file.
+        self.configs: Dict[str, Any] = {}
 
     def load(self) -> None:
-        """Loads `config.toml` to this instance.
-        Raises `IOError` if any io errors occurr during the load.
-        Raises `toml.DecodeError` if target file's syntax is checked invalid by toml.
+        """Load configurations from the file `configs_filepath`.
+        Raises :exec:`IOError` or :exec:`yaml.error.YAMLError` any io errors or yaml
+        syntax errors occurr.
         """
         try:
-            self.configs = toml.load(open(self.CONFIGS_FILENAME))
-        except (IOError, toml.TomlDecodeError) as err:
-            logger.critical(
-                "Failed to loads {0}: {1}".format(self.CONFIGS_FILENAME, err)
+            self.configs = yaml.load(open(self.configs_filepath))
+        except (IOError, yaml.error.YAMLError) as exc:
+            logger.critical("Failed to load {0}".format(self.configs_filepath))
+            raise exc
+
+    @cached_property
+    def site(self) -> Dict[str, Any]:
+        """Returns the configuration section `site`."""
+        return self.configs.get("site", {})
+
+    @cached_property
+    def site_name(self) -> str:
+        """Returns the configured `site.name`."""
+        return self.site.get("name", "")
+
+    @cached_property
+    def site_description(self) -> str:
+        """Returns the configured `site.description`."""
+        return self.site.get("description", "")
+
+    @cached_property
+    def site_theme(self) -> str:
+        """Returns the configured `site.theme`."""
+        return self.site.get("theme", "default")
+
+    @cached_property
+    def author(self) -> Dict[str, Any]:
+        """Returns the configured `author`."""
+        return self.configs.get("author", {})
+
+    @cached_property
+    def author_name(self) -> str:
+        """Returns the configured `author.name`."""
+        return self.author.get("name", "")
+
+    @cached_property
+    def author_email(self) -> str:
+        """Returns the configured `author.email`."""
+        return self.author.get("email", "")
+
+    @cached_property
+    def author_description(self) -> str:
+        """Returns the configured `author.description`."""
+        return self.author.get("description", "")
+
+    @cached_property
+    def disqus(self) -> str:
+        """Returns the configured section `disqus`."""
+        return self.configs.get("disqus", {})
+
+    @cached_property
+    def disqus_enable(self) -> bool:
+        """Returns the configured `disqus.enable`."""
+        return self.disqus.get("enable", False)
+
+    @cached_property
+    def disqus_shortname(self) -> str:
+        """Returns the configured `disqus.shortname`."""
+        return self.disqus.get("shortname", "")
+
+
+###
+# Models
+###
+
+
+@dataclass
+class Site:
+    """Site is an abstraction of model site."""
+
+    #: `name` is the name of this site which is read from configuration.
+    name: str
+    #: `description` is the description of this site which is read from configuration.
+    description: str
+    #: `theme` is the theme path  of this site which is read from configuration.
+    theme: str = "default"
+
+    @classmethod
+    def from_cofnigs(cls, configs: Configs) -> "Site":
+        """Constructs a :class:`Site` instance from given `configs`."""
+        return cls(
+            name=configs.site_name,
+            description=configs.site_description,
+            theme=configs.site_theme,
+        )
+
+
+@dataclass
+class Author:
+    """Author is an abstraction of model author."""
+
+    #: `name` is the name of this author which is read from configuration.
+    name: str
+    #: `email` is the email of this author which is read from configuration.
+    email: str
+
+    @classmethod
+    def from_cofnigs(cls, configs: Configs) -> "Author":
+        """Constructs a :class:`Author` instance from given `configs`."""
+        return cls(name=configs.author_name, email=configs.author_email)
+
+    @cached_property
+    def gravatar_id(self) -> str:
+        """Returns the gravatar id of this author by email."""
+        return hashlib.md5(self.email.encode("utf8")).hexdigest()
+
+
+@dataclass
+class Post:
+    """Post is an abstraction of model post."""
+
+    name: str
+    title: str
+    datetime: datetime.datetime
+    markdown: str
+    html: str
+    summary: str
+    filepath: str
+    title_pic: str
+
+
+###
+# Parser
+###
+
+
+class MarkdownRenderer(mistune.Renderer):
+    """MarkdownRenderer inherits the :class:`mistune.Renderer` and implements a custom
+    markdown renderer for rux.
+    """
+
+    def _code_no_lexer(self, code: str) -> str:
+        """Parts of :meth:`block_code()` for the no language condition."""
+        return """<div class="highlight"><pre><code>{0}</code></pre></div>""".format(
+            mistune.escape(code.strip())
+        )
+
+    def block_code(self, code: str, lang: str) -> str:
+        """Overrides upper :meth:`mistune.Renderer.block_code()`."""
+        if not lang:
+            return self._code_no_lexer(code)
+        try:
+            lexer: pygments.lexer.Lexer = pygments.lexers.get_lexer_by_name(
+                lang, stripall=True
             )
-            raise err
+        except pygments.util.ClassNotFound:
+            logger.warning("pygments lexer not found for {0}, ignored", lang)
+            return self._code_no_lexer(code)
+        formatter: pygments.formatter.Formatter = pygments.formatters.HtmlFormatter()
+        return pygments.highlight(code, lexer, formatter)
 
-    @cached_property
-    def root(self) -> str:
-        """Returns the configured `root` in `config.toml`.
-        Which is the root path of this blog.
-        """
-        return self.configs.get("root", "")
 
-    @cached_property
-    def blog(self) -> MutableMapping[str, Any]:
-        """Returns the section `[blog]` in `configs.toml`, defaults to `{}`."""
-        return self.configs.get("blog", {})
-
-    @cached_property
-    def blog_name(self) -> str:
-        """Returns the configured `blog.name` in `config.toml`.
-        Which is the name of this blog, defaults to `"Untitled"`.
-        """
-        return self.blog.get("name", "Untitled")
-
-    @cached_property
-    def blog_description(self) -> str:
-        """Returns the configured `blog.description`, defaults to `""`."""
-        return self.blog.get("description", "")
-
-    @cached_property
-    def blog_theme(self) -> str:
-        """Returns the configured `blog.theme`, defaults to `"default"`.
-        Which configures the path of the theme directory relative to the root.'"""
-        return self.blog.get("theme", "default")
+class Parser:
+    pass
